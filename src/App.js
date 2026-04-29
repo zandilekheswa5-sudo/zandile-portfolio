8ff12051f6da24890f3bacd21e11998fd88c114e
Zandile Kheswa <zandilekheswa5@gmail.com>
2026-04-29 15:07:38 +0200

    fix: import social icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import profile from "./assets/zandile.jpg";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 function App() {
   const [active, setActive] = useState("home");
